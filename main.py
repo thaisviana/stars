c5f8e0d5a4f9385a56fda5d9b323b091d8d32f26
thaisviana <thais@hub.co>
2017-09-12 14:30:52 -0300

    mongoURL

--- main.py
+++ main.py
@@ -1,42 +1,44 @@
+import os
+
 from flask import Flask
 from flask import jsonify
 from flask import request
 from flask_pymongo import PyMongo
 
-from star import Star
+from student import Student
 
 app = Flask(__name__)
 
-app.config['MONGO_DBNAME'] = 'flask'
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/flask'
+app.config['MONGO_DBNAME'] = 'student'
+app.config['MONGO_URI'] = os.environ.get('MONGOHQ_URL')
 
 mongo = PyMongo(app)
 
-@app.route('/star', methods=['GET'])
-def get_all_stars():
-	star = mongo.db.stars
-	output = []
-	for s in star.find():
-		output.append(Star.jsonify(s))
-	return jsonify({'result' : output})
+@app.route('/student', methods=['GET'])
+def get_all():
+    student = mongo.db.student
+    output = []
+    for s in student.find():
+        output.append(Student.jsonify(s))
+    return jsonify({'result': output})
 
-@app.route('/star/', methods=['GET'])
-def get_one_star(name):
-	star = mongo.db.stars
-	s = star.find_one({'name' : name})
-	if s:
-		return jsonify({'result' : Star.jsonify(s)})
-	return jsonify({'result' : "No such name"})
+@app.route('/student/', methods=['GET'])
+def get_one(student_id):
+    student = mongo.db.student
+    s = student.find_one({'student_id': student_id})
+    if s:
+        return jsonify({'result': Student.jsonify(s)})
+    return jsonify({'result': "No such name"})
 
-@app.route('/star', methods=['POST'])
-def add_star():
-	star = mongo.db.stars
-	
-	data = Star.jsonify(request.form)
-	star_id = star.insert(data)
-	
-	new_star = star.find_one({'_id': star_id })
-	return jsonify({'result' : Star.jsonify(new_star)})
+@app.route('/student', methods=['POST'])
+def add():
+    student = mongo.db.student
+
+    data = Student.jsonify(request.form)
+    student_id = student.insert(data)
+
+    new_student = student.find_one({'_id': student_id })
+    return jsonify({'result': Student.jsonify(new_student)})
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ stars_main.py
@@ -0,0 +1,46 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_pymongo import PyMongo
+
+from star import Star
+
+app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'flask'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/flask'
+
+mongo = PyMongo(app)
+
+
+@app.route('/star', methods=['GET'])
+def get_all_stars():
+    star = mongo.db.stars
+    output = []
+    for s in star.find():
+        output.append(Star.jsonify(s))
+    return jsonify({'result': output})
+
+
+@app.route('/star/', methods=['GET'])
+def get_one_star(name):
+    star = mongo.db.stars
+    s = star.find_one({'name': name})
+    if s:
+        return jsonify({'result': Star.jsonify(s)})
+    return jsonify({'result': "No such name"})
+
+
+@app.route('/star', methods=['POST'])
+def add_star():
+    star = mongo.db.stars
+
+    data = Star.jsonify(request.form)
+    star_id = star.insert(data)
+
+    new_star = star.find_one({'_id': star_id})
+    return jsonify({'result': Star.jsonify(new_star)})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
