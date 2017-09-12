20d30e7ae0a652188d3ecf0fc20ee0ca50c25cbe
thaisviana <thais@hub.co>
2017-09-12 16:37:58 -0300

    production_config

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
 import os
 
+
 class Config(object):
     MONGO_URI = os.environ.get('MONGOHQ_URL')
     DEBUG = False
     TESTING = False
     DATABASE_URI = MONGO_URI
+
+
+class ProductionConfig(Config):
+    DATABASE_URI = os.environ.get('MONGOHQ_URL')
